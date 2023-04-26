9edeb7b78785664624b82bb60ac2080b57999403
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-04-25 17:26:40 -0700

    Task Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,23 @@
-#include <stdio.h>
 #include "main.h"
 
-
 /**
-  *main - Entry point
-  *Description: "prints _putchar to stdout"
-  *Return: always 0 (success)
-  */
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
-int _putchar(char c);
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
