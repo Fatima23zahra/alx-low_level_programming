9c38da505e1f3f20ec8b78e5c40b9f370d4b27fc
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-05-02 16:18:48 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
-  * _strlen - Returns the length of a string
- * @s: The string to be measured
- *
+ * _strlen - Returns the length of a string
+ * @s: The string
  * Return: The length of the string
  */
 int _strlen(char *s)
