c0a5a4f3aeabf453e1119e8d1647643726e50f57
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-05-02 18:47:25 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ *@s: string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		long++;
+		s++;
+	}
+	return (longi);
+}
