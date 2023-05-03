ba8d62acf809d8cbd8de18818e4f03e338195504
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-05-02 17:48:12 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,14 @@
 void print_rev(char *s)
 {
 	int longi = 0;
-int 0;
+int o;
 
 	while (*s != '\0')
 	{
 		longi++;
-		s++,
+		s++;
 	}
-	S--;
+	s--;
 	for (0 = longi; o > 0; o--)
 	{
 		_putchar(*s);
