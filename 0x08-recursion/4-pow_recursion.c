33a51ff70a28f5a3d147e41760b725de5bae8e2b
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-05-18 14:26:12 -0700

    change

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,9 +9,15 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int result = x;
+
 	if (y < 0)
 		return (-1);
-	if (y == 0)
+
+	else if (y == 0)
 		return (1);
-	return(x* _pow_recursion(x, y - 1));
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
 }
