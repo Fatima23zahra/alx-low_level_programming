23e0d406f5bdc2b483fd28a8be9f6c684813885b
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-05-22 14:38:22 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,11 +9,12 @@
   * @argv: pointer to an arry of command line arguments.
   * Return: Always 0, non-zero-fail.
   */
-int main(int argc, char *argv[]) {
-   if (argc == 2) {
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
 		int i, leastcents = 0, money = atoi(argv[1]);
 		int cents[] = {25, 10, 5, 2, 1};
-		
 		for (i = 0; i < 5; i++)
 		{
 			if (money >= cents[i])
@@ -23,7 +24,7 @@ int main(int argc, char *argv[]) {
 				if (money % cents[i] == 0)
 				{
 					
-					break;
+	break;
 				}
 			}
 			
@@ -35,5 +36,5 @@ int main(int argc, char *argv[]) {
 			return (1);
 		}
 		
-		return (0);
+return (0);
 }
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: imports name
+ * @age: imports age
+ * @owner: imports the name of the owner
+ * 
+ * description: just a lone dog struct in a big kitty world
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
