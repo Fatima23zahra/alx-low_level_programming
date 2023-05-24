023f2a08641c3ef1b793ab468186eea3eb9c90f9
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-05-24 07:25:39 -0700

    a

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - the logic code for the school student
+ * @argc: the num or args
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*calc)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	calc = get_op_func(argv[2]);
+
+	if (calc == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((atoi(argv[3]) == 0) && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", calc(a, b));
+
+return (0);
+}
