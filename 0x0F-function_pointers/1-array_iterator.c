fe5e670339c9c6a0b24552fad0aed5a9de5687df
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-05-24 06:11:28 -0700

    a

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints the arrays through a function pointer
+ * @array: the int array
+ * @action: the function pointer
+ * @size: uses size_t to calculate the size of array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int index;
+
+	if ((array != NULL) && (action != NULL))
+	{
+		for (index = 0; index < size; index++)
+		{
+			(*action)(array[index]);
+		}
+	}
+}
