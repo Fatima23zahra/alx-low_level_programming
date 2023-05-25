495f0ff12e0283e3628f73bdc205b1602e6a52e2
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-05-25 08:29:25 -0700

    a

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,19 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct print_fn - a struct print_fn
+ *
+ * @format: the format specifier
+ * @fn: the print function
+ *
+ */
+typedef struct print_fn
+{
+	char format;
+	int (*fn)(va_list);
+} print_fn_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
