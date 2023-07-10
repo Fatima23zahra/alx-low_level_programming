1bf08c860e4455ce4d0a009a7dec8cf6ba03804e
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2023-07-10 16:40:58 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,19 +15,19 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (text_content == NULL)
 		text_content = "";
 	}
+	if (filename == NULL)
 	{
-		if (filename == NULL)
 		return (-1);
 	}
 	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 	{
-		if (fd == -1)
 		return (-1);
 	}
 	for (lettercount = 0; text_content[lettercount] != '\0'; lettercount++)
 	write_d = write(fd, text_content, lettercount);
+	if (write_d == -1)
 	{
-		if (write_d == -1)
 		return (-1);
 	}
 
